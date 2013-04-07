a3f7846fc05105024ba6f7d73ab486a5a7fb6791
Ingo Bauersachs <ingo@jitsi.org>
2013-04-07 19:16:36 +0200

    Extract and package-rename from Skein3Fish-Project

---
+++ src/main/java/org/jitsi/bccontrib/params/ParametersForSkein.java
@@ -0,0 +1,51 @@
+package org.jitsi.bccontrib.params;
+
+import org.bouncycastle.crypto.CipherParameters;
+
+public class ParametersForSkein implements CipherParameters {
+
+    public final static int Skein256 = 256;
+    public final static int Skein512 = 512;
+    public final static int Skein1024 = 1024;
+    
+    private int macSize;
+    private int stateSize;
+    private CipherParameters parameters;
+
+    /**
+     * Skein parameters for Skein MAC.
+     * 
+     * @param parameters
+     *     This must be a KeyParameter instance that holds the key. 
+     * @param stateSize
+     *     The Skein state size to use.
+     * @param macSize
+     *     The requested Skein MAC output size in bits.
+     */
+    public ParametersForSkein(
+            CipherParameters    parameters,
+            int                 stateSize,
+            int                 macSize)
+    {
+        this.macSize = macSize;
+        this.stateSize = stateSize;
+        this.parameters = parameters;
+    }
+
+    public int getMacSize()
+    {
+        return macSize;
+    }
+
+    public int getStateSize()
+    {
+        return stateSize;
+    }
+
+    public CipherParameters getParameters()
+    {
+        return parameters;
+    }
+
+
+}
---
+++ src/main/java/org/jitsi/bccontrib/params/ParametersForThreefish.java
@@ -0,0 +1,49 @@
+package org.jitsi.bccontrib.params;
+
+import org.bouncycastle.crypto.CipherParameters;
+
+public class ParametersForThreefish implements CipherParameters {
+    public final static int Threefish256 = 256;
+    public final static int Threefish512 = 512;
+    public final static int Threefish1024 = 1024;
+    
+    private int stateSize;
+    private CipherParameters parameters;
+    private long[] tweak;
+
+    public ParametersForThreefish(
+            CipherParameters    parameters,
+            int                 stateSize,
+            long[]              tweak)
+    {
+        this.stateSize = stateSize;
+        this.parameters = parameters;
+        if (tweak != null) {
+            this.tweak = new long[2];
+            this.tweak[0] = tweak[0];
+            this.tweak[1] = tweak[1];
+        }
+    }
+
+    /**
+     * @return the stateSize
+     */
+    public int getStateSize() {
+        return stateSize;
+    }
+
+    /**
+     * @return the parameters
+     */
+    public CipherParameters getParameters() {
+        return parameters;
+    }
+
+    /**
+     * @return the tweak
+     */
+    public long[] getTweak() {
+        return tweak;
+    }
+
+}
